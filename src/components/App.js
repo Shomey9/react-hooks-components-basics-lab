e819e79c1a6325e363f03d04dcc12e045e3ddbc0
Muhammad Rendusara <hadyanrendusara@yahoo.com>
2021-11-29 20:29:37 -0600

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,19 @@
 import React from "react";
-
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
+import About from "./About";
+import NavBar from "./Navbar";
+import Home from "./Home";
 
 {/* write an <About> component here */}
-
+<About />
 
 function App() {
   return (
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
